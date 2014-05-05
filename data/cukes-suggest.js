73b197f73d342f85328079f334f62be14242319d
Sander van Vliet <sauron@barad-dur.nl>
2014-05-05 14:01:24 +0200

    added suggest box

---
+++ data/cukes-suggest.js
@@ -0,0 +1,5 @@
+{
+	'givens': ["the date is today", "the internet is down"],
+	//'whens':["I browse to Google", "I view the list"],
+	//'thens':["I see the Google start page", "the list contains four features", "the feature item indicates four scenarios", "I see an error message"]
+}
