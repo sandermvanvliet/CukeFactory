f3ff0d2f7eef8ed6c7c28fc8f9a0ceba6942902d
Sander van Vliet <sauron@barad-dur.nl>
2014-05-07 08:19:33 +0200

    Refactored cukeservice
    Added configuration object

--- node/cukeservice.js
+++ node/cukeservice.js
@@ -1,10 +1,14 @@
 var restify = require('restify');
 
-var ip_addr = '127.0.0.1';
-var port = '8080';
+var configuration = {
+	listen_ip_addr: '127.0.0.1',
+	port: '8080',
+	name: 'cukeservice',
+	cukesPath: '../data/features.json'
+};
 
 var server = restify.createServer({
-		name : "cukeservice"
+		name : configuration.name
 });
 
 server.use(restify.queryParser());
@@ -12,11 +16,10 @@ server.use(restify.bodyParser());
 server.use(restify.CORS());
 
 var PATH = '/cukes'
-var cukesFile = '../data/features.json';
 
 server.get({path : PATH, version: '0.0.1' }, listAllCukes);
 
-server.listen(port, ip_addr, function() {
+server.listen(configuration.listen_port, configuration.listen_ip_addr, function() {
 	console.log('%s listening at %s', server.name, server.url);
 });
 
@@ -24,7 +27,7 @@ function listAllCukes(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 
 	var fs = require('fs');
-	fs.readFile(cukesFile, 'utf8', function(err, data) {
+	fs.readFile(configuration.cukesPath, 'utf8', function(err, data) {
 		if(err) throw err;
 
 		var jsObjData = JSON.parse(data);
