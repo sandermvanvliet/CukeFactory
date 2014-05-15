5cebbe75a145738b00ba332aec86cba09a9a224a
Sander van Vliet <sauron@barad-dur.nl>
2014-05-15 16:54:11 +0200

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <% pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */',
+			},
+			build: {
+				files: {
+					'js/cukefactory.min.js': [
+		'js/viewmodels/step.js',
+		'js/viewmodels/scenario.js',
+		'js/viewmodels/feature.js',
+		'js/controllers/viewmodels.js'
+					],
+					'js/typeahead.min.js': [
+		'js/controllers/typeahead.js'
+					]
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['uglify']);
+};
